0220c399050e47fcc17075d25b9c806886397505
mustafakemal1729 <experimus1@gmail.com>
2024-03-04 10:16:53 +0100

    register page

--- frontend/src/components/Pagination.vue
+++ frontend/src/components/Pagination.vue
@@ -40,4 +40,4 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,21 +1,24 @@
 <template>
     <section>
         <div class="container">
-            <form class="mt-5" @submit="submitForm">
-                <div class="row justify-content-center">
+            <form class="mt-3" @submit="submitForm">
+                <div class="row justify-content-center custom">
                     <!-- Username Field (Medium and Larger Screens) -->
                     <div class="col-md-6 col-8 mb-3">
                         <label for="username" class="form-label">Username</label>
-                        <input type="text" class="form-control" id="username" name="username" v-model="formData.username"
-                            required>
+                        <input type="text" class="form-control" id="username" name="username"
+                            v-model="formData.username" required>
                     </div>
                 </div>
+
+                {{ formData.username }}
 
                 <div class="row justify-content-center">
                     <!-- Email Field (Medium and Larger Screens) -->
                     <div class="col-md-6 col-8 mb-3">
                         <label for="email" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="email" name="email" v-model="formData.email" required>
+                        <input type="email" class="form-control" id="email" name="email" v-model="formData.email"
+                            required>
                     </div>
                 </div>
 
@@ -25,66 +28,6 @@
                         <label for="password" class="form-label">Password</label>
                         <input type="password" class="form-control" id="password" name="password"
                             v-model="formData.password" required>
-                    </div>
-                </div>
-
-                <!-- Gender Field -->
-                <div class="row justify-content-center">
-                    <div class="col-md-6 col-8 mb-3">
-                        <h5>Gender</h5>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="gender" value='male' id="male"
-                            v-model="formData.gender">
-                            <label class="form-check-label" for="male">
-                                Male
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="gender" value='female' id="female"
-                            v-model="formData.gender">
-                            <label class="form-check-label" for="female">
-                                Female
-                            </label>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Age Field -->
-                <div class="row justify-content-center">
-                    <div class="col-md-6 col-8 mb-3">
-                        <label for="age" class="form-label">Age:</label>
-                        <input class="form-control" id="age" v-model.number="formData.age" required>
-                    </div>
-                </div>
-
-                <!-- Programming Field -->
-                <div class="row justify-content-center">
-                    <div class="col-md-6 col-8 mb-3">
-                        <h5>Which Programming Language(s) you know?</h5>
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="java" v-model="formData.languages" id="java">
-                            <label class="form-check-label" for="java">
-                                Java
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="c++" v-model="formData.languages" id="c++">
-                            <label class="form-check-label" for="c++">
-                                C++
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="python" v-model="formData.languages" id="python">
-                            <label class="form-check-label" for="python">
-                                Python
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="rust" v-model="formData.languages" id="rust">
-                            <label class="form-check-label" for="rust">
-                                Rust
-                            </label>
-                        </div>
                     </div>
                 </div>
 
@@ -109,20 +52,14 @@ export default {
                 username: '',
                 email: '',
                 password: '',
-                gender: '',
-                age: null,
-                languages: [],
-            }
-        }
-    },
-
-    methods: {
-        submitForm(event){
-            event.preventDefault();
-            console.log("formData:", this.formData)
+            },
         }
     }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.custom {
+    margin-top: 10px;
+}
+</style>
