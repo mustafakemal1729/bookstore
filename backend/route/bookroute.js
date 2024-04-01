492404bbdb42087f57b63354d5b9aeb50028aae7
mustafakemal1729 <experimus1@gmail.com>
2024-04-01 11:36:20 +0200

    start over

--- backend/route/bookroute.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import * as bookcontroller from "./bookcontroller.js";
-const router = express.Router();
-
-router.get("/", bookcontroller.getAllBooks);
-
-export default router;
